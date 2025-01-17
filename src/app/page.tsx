d46ca44b70029d48cd61e230d21e1280332d5ffd
Trisha S Shetty <shettytrisha197@gmail.com>
2025-01-17 14:49:25 +0530

    [style] Added Logo image as bacground

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
-const Page = () => {
+import React from 'react'
+
+const page = () => {
   return (
-    <div>
+    <div
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '100vh',
+        backgroundImage: `url('/logo.png')`,
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        opacity: 0.07
+      }}
+    >  
     </div>
   );
 };
+export default page;
 
-export default Page;
+
+
